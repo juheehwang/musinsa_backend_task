74a9070bdf56e0c9d770f2c13048157d62a0ab22
juheehwang <fhqj21@gmail.com>
2025-04-18 16:13:30 +0900

    3번 카테고리 이름으로 최저,치고 가격 브랜드와 가격 조회

--- src/main/java/com/musinsa/task/entity/ProductRepository.java
+++ src/main/java/com/musinsa/task/entity/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.musinsa.task.entity;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
     List<Product> findByCategoryOrderByPriceAsc(Category category);
+
+    List<Product> findByCategoryOrderByPriceDesc(Category category);
 
 }
